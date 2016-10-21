95724a21f0b5fdab65eeb87fa85cfc520c0aa35f
Eduardo <Eduardo@192.168.1.74>
2016-10-20 23:44:05 -0500

    https://github.com/EGiadans/Proyecto-Parcial-2.git

---
+++ src/problema/pkg6/Problema6.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema.pkg6;
+import java.util.*;
+/**
+ *
+ * @author Eduardo
+ */
+public class Problema6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int h,p,x;
+        h=getHoras();
+        p=getPaga();
+        x=horaExt(h);
+        sueldo(h,p,x);
+    }
+    public static int getHoras(){
+        int horas;
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Introduce cantidad de horas trabajadas");
+        horas=teclado.nextInt();
+        return horas;
+    }
+    public static int getPaga(){
+        int paga;
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Introduce paga por hora del empleado");
+        paga=teclado.nextInt();
+        return paga;
+    }
+    public static void sueldo(int paga, int horas,int horasExtra){
+        double sueldo=((paga*horas)+(paga*horasExtra));
+        System.out.println("El sueldo del empleado es: $"+sueldo);
+    }
+    
+    public static int horaExt(int horas){   
+        int horasExtra=0;
+        if (horas>40){
+            horasExtra=(horas-40);
+            
+        }if (horasExtra<=8){
+                    horasExtra=horasExtra*2;
+            }else if(horasExtra>8){
+                            horasExtra=horasExtra*3;
+                            System.out.println(horasExtra);
+            }    
+    return horasExtra;
+    }
+}
